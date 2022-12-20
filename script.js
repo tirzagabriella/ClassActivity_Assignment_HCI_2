724cb3db8e9aa4b097d54d7490fb13c1e52678b5
tirzagabriella <74703140+tirzagabriella@users.noreply.github.com>
2022-12-20 14:00:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Add your answer here
+$(function() {
+    $('.lesson').hover(
+      function() {
+        $(this).find('.text-contents').addClass('text-active');
+      },
+      function() {
+        $(this).find('.text-contents').removeClass('text-active');
+      }
+    );
+  });
